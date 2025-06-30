4c1f37c2ac7df445a24fb425760af68868fc3e41
KHJ-hyojun <kgywns03@gmail.com>
2025-06-30 23:40:44 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int N;
+	std::cin >> N;
+	if ((N % 4 == 0 && N % 100 != 0) || (N % 400 == 0)) {
+		std::cout << 1 << std::endl;
+
+	}
+	else
+		std::cout << 0 << std::endl;
+
+}
