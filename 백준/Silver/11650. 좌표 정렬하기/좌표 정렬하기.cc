3e4bb81faeceeb50bfbbc5174861ecf5b980b522
KHJ-hyojun <kgywns03@gmail.com>
2025-09-26 14:07:13 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 100 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmd(pair<int,int> a, pair<int,int> b) {
+	if (a.first == b.first) {
+		return a.second < b.second;
+	}
+	return a.first < b.first;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	vector<pair<int, int>> dot_v(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> dot_v[i].first >> dot_v[i].second;
+	}
+
+	sort(dot_v.begin(), dot_v.end(), cmd);
+	
+	for (int i = 0; i < N; i++) {
+		cout << dot_v[i].first << " " << dot_v[i].second << "\n";
+	}
+}
