e2610d512bca131af056d9c854866c5ebdd87cbc
KHJ-hyojun <kgywns03@gmail.com>
2025-10-28 10:35:43 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char arr[51][51];
+
+string WB[8] = {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+};
+
+string BW[8] = {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+};
+
+int f_white(int x, int y)
+{
+    int result = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (arr[x + i][y + j] != WB[i][j])
+                result++;
+        }
+    }
+    return result;
+}
+
+int f_black(int x, int y)
+{
+    int result = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (arr[x + i][y + j] != BW[i][j])
+                result++;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, M;
+    int result = 64;
+    int white, black;
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+            cin >> arr[i][j];
+    }
+
+    for (int i = 0; i <= N - 8; i++)
+    {
+        for (int j = 0; j <= M - 8; j++)
+        {
+            white = f_white(i, j);
+            black = f_black(i, j);
+
+            result = min(result, min(white, black));
+        }
+    }
+    cout << result << '\n';
+
+}
