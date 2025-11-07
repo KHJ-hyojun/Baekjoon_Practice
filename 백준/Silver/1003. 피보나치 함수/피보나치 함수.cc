e8087f9e28a81e914c57781a0fb4b8cbbd228555
KHJ-hyojun <kgywns03@gmail.com>
2025-11-07 22:45:10 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int dp0[41] = { 0 };
+	int dp1[41] = { 0 };
+
+	dp0[0] = 1, dp0[1] = 0;
+	dp1[0] = 0, dp1[1] = 1;
+
+	for (int i = 2; i <= 40; i++)
+	{
+		dp0[i] = dp0[i - 1] + dp0[i - 2];
+		dp1[i] = dp1[i - 1] + dp1[i - 2];
+	}
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int k;
+		cin >> k;
+		cout << dp0[k] << " " << dp1[k] << "\n";
+	}
+
+}
